e2757e45728643b23d618472af4f95b01b8d6a44
asukas00 <jessere2025@gmail.com>
2025-12-22 09:33:10 -0500

    test

--- MC.py
+++ MC.py
@@ -1,5 +1,9 @@
 
 class Tank:
-    def __init__(self, health, attack, armor, level):
-        self.health =
-        self.attack= 
+    def __init__(self, health, attack, armor, level, exp):
+        self.health = health
+        self.attack = attack
+        self.armor = armor 
+        self.level = level
+        self.exp = exp
+        
