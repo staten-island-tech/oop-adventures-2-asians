8e64113e2e03c45a5701e5bfcb573322cfbf9c67
asukas00 <jessere2025@gmail.com>
2025-12-22 10:07:45 -0500

    Update MC.py

--- MC.py
+++ MC.py
@@ -1,9 +1,24 @@
 
 class Tank:
-    def __init__(self, health, attack, armor, level, exp):
+    def __init__(self, name, health, attack, armor, level, exp, maxhealth, defense_ability):
+        self.name = name
         self.health = health
         self.attack = attack
         self.armor = armor 
         self.level = level
         self.exp = exp
+        self.maxhealth = maxhealth
+        self.defense_ability = defense_ability
+    def takedamage(self, damage, damage_type = 'physical'):
+        if damage_type == 'magical':
+            damage_after_armor = damage * 0.75
+        else:
+            damage_after_armor = max(0, damage - self.armor)
+            
+        self.health -= damage_after_armor
+        self.health = max(0, self.health)
+    def attack_dmg(self, damage):
+
         
+
+
