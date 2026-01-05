7f7e029e5b5a1099944d8201b7b5655173d00e8c
Kanashii-Kito <ERicc468@nycstudents.net>
2026-01-05 09:47:16 -0500

    Update E.py

--- E.py
+++ E.py
@@ -1,7 +1,7 @@
 
 """ idiot is goblin """
-""" dumb is wizard """
-""" retard is enemies_tank """
+""" dumb is skellyton """
+""" retard is robber """
 
 class idiot:
     def __init__(self, name, health, attack, armor):
