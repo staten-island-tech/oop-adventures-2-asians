415cb2edef467003c2d68b7421fc690612ab63f6
asukas00 <jessere2025@gmail.com>
2026-01-07 09:53:55 -0500

    Shop 1

---
+++ shop.py
@@ -0,0 +1,22 @@
+
+class Shop:
+    def __init__(self, items)
+        self.items = ["Sean", "Leo"]
+    def weapons(Sword, Staff, Dagger, Bow, Shield, Axe, Wand):
+
+    
+
+    def potions(strength, defense, speed, heal, mana, luck):
+
+
+    
+
+    def rarity(uncommon, epic, mythic):
+
+
+    
+    def buy(self, coin, item_selected):
+        if coin > item_selected
+    
+    def sell():
+Aayans = shop(["Sean", "Leo"])
