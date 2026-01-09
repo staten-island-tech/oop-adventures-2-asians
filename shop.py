ddabc6b062f020fc2cf06349e378f941dc69e490
asukas00 <jessere2025@gmail.com>
2026-01-09 10:17:51 -0500

    New one

--- shop.py
+++ shop.py
@@ -15,7 +15,7 @@ class Shop:
             selected_items_cart.append(inv)
         
     def sell(self, items):
-        
+      return  
 
 class Potions():
     def __init__(self,title, price, rarity, stats, stock):
@@ -24,8 +24,10 @@ class Potions():
         self.rarity = rarity
         self.stats = stats
         self.stock = stock
-    def items(self, Heal, Charm, Luck, Stength):
-
+Heal = Potions()
+Charm = Potions()
+Luck = Potions()
+Stength = Potions()
 
 
 
@@ -36,17 +38,84 @@ class Weapons:
         self.rarity = rarity
         self.stats = stats
         self.stock = stock
-    def items(self, Sword, Bow, OffhandShield, Dagger, Battleaxe, MajicStaff, Wand):
+
+
+Sword = Weapons()
+Bow = Weapons()
+OffhandShield = Weapons()
+Dagger = Weapons()
+Battleaxe = Weapons()
+MajicStaff = Weapons()
+Wand = Weapons()
 
 
 
 
 
 class Armor():
-    def __init__(self, title, price, rarity, stats, stock):
+    def __init__(self, title, price, rarity, stats, stock, weight):
         self.title = title
         self.price = price
         self.rarity = rarity
         self.stats = stats
         self.stock = stock
-helmet = []
+        self.weight = weight
+
+
+# light uncommon armor
+Helmet = Armor("Light_Uncommon_Helmet", 10, "uncommon", )
+Chestplate = Armor("Light_Uncommon_Chestplate",)
+Pants = Armor("Light_Uncommon_Pants",)
+Boots = Armor("Light_Uncommon_Boots",)
+
+# light epic armor
+Helmet = Armor("Light_Epic_Helmet")
+Chestplate = Armor("Light_Epic_Chestplate")
+Pants = Armor("Light_Epic_Pants")
+Boots = Armor("Light_Epic_Boots")
+
+# light mythic armor
+Helmet = Armor("Light_Mythic_Helmet")
+Chestplate = Armor("Light_Mythic_Chestplate")
+Pants = Armor("Light_Mythic_Pants")
+Boots = Armor("Light_Mythic_Boots")
+
+# medium uncommon armor
+Helmet = Armor("Medium_Uncommon_Helmet")
+Chestplate = Armor("Medium_Uncommon_Chestplate")
+Pants = Armor("Medium_Uncommon_Pants")
+Boots = Armor("Medium_Uncommon_Boots")
+
+# medium epic armor
+Helmet = Armor("Medium_Epic_Helmet")
+Chestplate = Armor("Medium_Epic_Chestplate")
+Pants = Armor("Medium_Epic_Pants")
+Boots = Armor("Medium_Epic_Boots")
+
+# medium mythic armor
+Helmet = Armor("Medium_Mythic_Helmet")
+Chestplate = Armor("Medium_Mythic_Chestplate")
+Pants = Armor("Medium_Mythic_Pants")
+Boots = Armor("Medium_Mythic_Boots")
+
+# heavy uncommon armor
+Helmet = Armor("Heavy_Uncommon_Helmet")
+Chestplate = Armor("Heavy_Uncommon_Chestplate")
+Pants = Armor("Heavy_Uncommon_Pants")
+Boots = Armor("Heavy_Uncommon_Boots")
+
+# heavy epic armor
+Helmet = Armor("Heavy_Epic_Helmet")
+Chestplate = Armor("Heavy_Epic_Chestplate")
+Pants = Armor("Heavy_Epic_Pants")
+Boots = Armor("Heavy_Epic_Boots")
+
+# heavy mythic armor
+Helmet = Armor("Heavy_Mythic_Helmet")
+Chestplate = Armor("Heavy_Mythic_Chestplate")
+Pants = Armor("Heavy_Mythic_Pants")
+Boots = Armor("Heavy_Mythic_Boots")
+
+
+
+
