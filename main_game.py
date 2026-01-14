bc2e8c079add29e096a63d536bd5efdb751947f8
asukas00 <jessere2025@gmail.com>
2026-01-14 10:02:52 -0500

    Commit for main game

--- main_game.py
+++ main_game.py
@@ -1,6 +1,18 @@
-from MC import MC
-from E import E
-from inventory import inventory
+from MC import *
+from E import *
+from inventory import *
+def enter_game():
+    game = True
+    while game == True:
+        user = input("What is your name?").strip()
+        if user == (""):
+            print("that is not a name idiot")
+            user = input("enter a name again:")
+        user_class = input(f"What class do you want to be {user}?   (mage, tank, healer): ").lower()
+        if user_class not in ["mage", "tank" , "healer"]:
+            user_class=input("Invalid action. Please choose a correct class.").lower()
+        game = False
+    
 
-input("What is your name?")
-input("What class do you want to be(Mage, Tank, Healer): ")
+enter_game()
+
