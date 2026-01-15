8cd5b2505de01ec9cff62591006388c0dd611007
asukas00 <jessere2025@gmail.com>
2026-01-15 09:56:30 -0500

    game

--- main_game.py
+++ main_game.py
@@ -3,24 +3,25 @@ from E import *
 from inventory import *
 def enter_game():
     game = True
-    while game == True:
-        user = input("What is your name?").strip()
-        if user == (""):
-            print("that is not a name idiot")
-            user = input("enter a name again:")
-        user_class = input(f"What class do you want to be {user}?   (mage, tank, healer, warrior): ").lower()
-        if user_class not in ["mage", "tank" , "healer", "warrior"]:
-            user_class=input("Invalid action. Please choose a correct class.").lower()
-            if input == "mage":
-                print(f"Congrats, {user} your are a mage")
-            elif input == "tank":
-                print(f"Congrats, {user} your are a tank")
-            elif input == "healer":
-                print(f"Congrats, {user} your are a healer")
-            elif input == "warrior":
-                print(f"Congrats, {user} your are a warrior")
+    while game:
+        user = input("What is your name? ").strip()
+        if user == "":
+            print("That is not a name, idiot.")
+            user = input("Enter a name again: ")
+        
+        user_class = input(f"What class do you want to be, {user}? (mage, tank, healer, warrior): ").lower()
+        
+        while user_class not in ["mage", "tank", "healer", "warrior"]:
+            user_class = input("Invalid action. Please choose a correct class: ").lower()
+        
+        if user_class == "mage":
+            print(f"Congrats, {user}, you are a mage!")
+        elif user_class == "tank":
+            print(f"Congrats, {user}, you are a tank!")
+        elif user_class == "healer":
+            print(f"Congrats, {user}, you are a healer!")
+        elif user_class == "warrior":
+            print(f"Congrats, {user}, you are a warrior!")
+        
         game = False
-    
-
 enter_game()
-
